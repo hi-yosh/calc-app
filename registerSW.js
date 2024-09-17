f0a587468be71924454ed4481775e5a61e7b7ae6
Yoshimitsu Miyagi <yoshimitsu.miyagi@gmail.com>
2024-09-17 06:00:05 +0000

    pwa test

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://hi-yosh.github.io/calc-app/sw.js', { scope: 'https://hi-yosh.github.io/calc-app/' })})}
